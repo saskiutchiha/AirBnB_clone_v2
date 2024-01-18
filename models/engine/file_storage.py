f0b31e97fc8e0aad159d0a4a161212bd4ecca36b
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-01-18 03:48:31 +0100

    removing obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,6 +24,11 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+    def delete(self, obj=None):
+        if obj != None:
+            for i in __objects:
+                if i == obj:
+                    __objects.pop(obj)
 
     def reload(self):
         """Loads storage dictionary from file"""
