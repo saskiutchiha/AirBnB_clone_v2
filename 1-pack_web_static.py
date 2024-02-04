abcfaa8409c2a0a86574aae307b565891a808dc3
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-04 23:49:14 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,8 +15,8 @@ def do_pack():
             date = datetime.now().strftime("%Y%m%d%H%M%S")
             if isdir("versions") is False:
                     local("mkdir versions")
-                    file_name = "versions/web_static_{}.tgz".format(date)
-                    local("tar -cvzf {} web_static".format(file_name))
-                    return file_name
-            except:
+            file_name = "versions/web_static_{}.tgz".format(date)
+            local("tar -cvzf {} web_static".format(file_name))
+            return file_name
+        except:
                     return None
