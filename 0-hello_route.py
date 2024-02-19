ad4c8fe566c226acb5ab5f7d1daaaa885ca7bb77
saskiutchiha <boulidamabdellah8@gmail.com>
2024-02-19 16:20:27 +0100

    ll

---
+++ 0-hello_route.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3 
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def helllo():
+    return "Hello HBNB!"
+if __name__ == "__main__":
+ app.run(host="0.0.0.0",port=5000)
