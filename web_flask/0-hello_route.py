8f6dec045de16b16fc41396ecdb32def9a6fbb4d
saskiutchiha <boulidamabdellah8@gmail.com>
2024-02-19 16:54:49 +0100

    l

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3 
+from flask import Flask
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+app = Flask(__name__)
+@app.route("/")
+def helllo():
+    return "Hello HBNB!"
+if __name__ == "__main__":
+ app.run(host="0.0.0.0",port=5000)
