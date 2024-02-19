a5b5da29d1915ec3a0fdba65ee7bfa8307292402
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-19 16:33:58 +0100

    Update 0-hello_route.py

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -1,5 +1,11 @@
 #!/bin/usr/python3 
 from flask import Flask
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
 app = Flask(__name__)
 @app.route("/")
 def helllo():
