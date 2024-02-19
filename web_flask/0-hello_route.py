33b3bfd2d77906027db04c8b9fd9dcf6025c1208
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-19 16:57:54 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,7 @@ Routes:
 app = Flask(__name__)
 @app.route("/")
 def helllo():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 if __name__ == "__main__":
  app.run(host="0.0.0.0",port=5000)
