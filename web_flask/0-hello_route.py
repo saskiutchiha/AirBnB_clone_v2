c7d9b8dc57cdffc8bbec9cc4787a171627682ddb
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-19 17:05:16 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,10 @@ Route '/' displays "Hello HBNB!"
 app = Flask(__name__)
 @app.route("/")
 def helllo():
-     """Displays 'Hello HBNB!'"""
+    """
+    This function is associated with the route '/'.
+    When this route is accessed, it returns the string 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 if __name__ == "__main__":
  app.run(host="0.0.0.0",port=5000)
