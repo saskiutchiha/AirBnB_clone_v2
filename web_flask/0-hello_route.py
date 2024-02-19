7481b8633031fc87c7e2c6455a0aeb4bb94d2eb0
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-20 00:22:12 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3 
-from flask import Flask
 """Starts Flask web app
 Listening on 0.0.0.0:5000
 Route '/' displays "Hello HBNB!"
 """
+
+from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
